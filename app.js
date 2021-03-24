d11ff035aaea5df4bf80851541da78e92c5771c2
Viktoryia Yatskova <epa03005@adobe.com>
2021-03-24 14:10:23 +0300

    fix: Styles for new task and remove text for Delete-button

--- app.js
+++ app.js
@@ -38,11 +38,11 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
+    editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
 
-    deleteButton.innerText="Delete";
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
