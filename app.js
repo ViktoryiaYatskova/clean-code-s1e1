d386167cc12930576183a6fc890357f20b49dd75
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-24 14:06:49 +0300

    refactor: adjust BEM according to rule 2.1 in html-and-css-extende.md

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("main-page__incomplete-tasks");//ul of #incomplete-tasks
+var completedTasksHolder=document.getElementById("main-page__completed-tasks");//completed-tasks
 
 
 //New task list item
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("incomplete-tasks__edit-mode");
     //If class of the parent is .edit-mode
     if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("incomplete-tasks__edit-mode");
 };
 
 
