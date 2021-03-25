9a5e78cdb876fbd6e686b602c7b77e5d6c2a2782
Igor Lyatskiy <igor.lyatskiy@gmail.com>
2021-03-25 19:14:15 +0300

    fix: rename some classes according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 var taskInput = document.getElementById("new-task");//Add a new task.
 var addButton = document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder = document.querySelector(".incompleted-tasks");//ul of #incompleted-tasks
-var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
+var completedTasksHolder = document.querySelector(".completed-tasks");//completed-tasks
 
 
 //New task list item
@@ -33,14 +33,14 @@ var createNewTaskElement = function (taskString) {
   var deleteButtonImg = document.createElement("img");//delete button image
 
   label.innerText = taskString;
-  label.className = 'task';
+  label.className = 'tasks-list__li__label';
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
   editInput.type = "text";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "tasks-list__li__button_edit";
 
   deleteButton.innerText = "Delete";
   deleteButton.className = "delete";
@@ -84,7 +84,7 @@ var editTask = function () {
 
   var editInput = listItem.querySelector('input[type=text]');
   var label = listItem.querySelector("label");
-  var editBtn = listItem.querySelector(".edit");
+  var editBtn = listItem.querySelector(".tasks-list__li__button_edit");
   var containsClass = listItem.classList.contains("editMode");
   //If class of the parent is .editmode
   if (containsClass) {
@@ -156,7 +156,7 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   console.log("bind list item events");
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
-  var editButton = taskListItem.querySelector("button.edit");
+  var editButton = taskListItem.querySelector("button.tasks-list__li__button_edit");
   var deleteButton = taskListItem.querySelector("button.delete");
 
 
