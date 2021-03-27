0b4fe1b0e09f1197fe998dee35ae123fe425e364
lansyfonseka <twinkstriker@gmail.com>
2021-03-27 21:14:21 +0300

    refactor: adjust HTML-formatting according to rule 3.1, 3.2, 3.3  in html-and-css.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("add-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
