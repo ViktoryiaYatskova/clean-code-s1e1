28d962ddc1c9debc65bdd429d09fb798b4c8d408
3xtZzy <38046904+VanQube@users.noreply.github.com>
2021-04-06 01:10:13 +0300

    refactor: JS-formating according to rule 2.1 BEM in html-and-css advanced

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-
+    listItem.classList.add('list__item')
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
