8a71f07318411e3946abbf111c709df3a7914cf7
Ali <mulwor.001@yandex.ru>
2022-05-23 12:20:46 +0300

    refactor: rule 3.1 - 3.3 try to use BEM - first

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='todo_task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="todo_task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
