d13301114c5da4c1b576ec61569e53d71b12fe95
Olga Karalkina <belocnejka52@gmail.com>
2022-05-31 00:38:15 +0300

    fix: edit for visual correspondence with original app

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="task__button-delete button";
     deleteButtonImg.src="./remove.svg";
+    deleteButtonImg.className="task__button-delete_img"
     deleteButton.appendChild(deleteButtonImg);
 
 
