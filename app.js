f051e9b2f3bcdcd3b23d03cfc2a78674527b3bfb
Dek1234 <syrse@mail.ru>
2022-05-31 01:21:34 +0300

    refactor: change quotes according to rule 1.3 in html-and-css

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ var addTask=function(){
 
 var editTask=function(){
     console.log("Edit Task...");
-    console.log("Change "edit" to "save"");
+    console.log("Change 'edit' to 'save'");
 
 
     var listItem=this.parentNode;
