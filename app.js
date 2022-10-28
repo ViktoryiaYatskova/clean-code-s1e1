0ee6772b68a41464000b34e11a93763e14f7efc0
OlegLepeshko <74596871+OlegLepeshko@users.noreply.github.com>
2022-10-28 16:34:23 +0300

    fix: fix CSS rule 3.1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var completedTasksHolder=document.getElementById("completedTasks");//completed-tasks
 
 
 //New task list item
