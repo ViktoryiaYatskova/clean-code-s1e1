8f97f43752fa99743e94b6e5dfd2f96b9b639015
OlegLepeshko <74596871+OlegLepeshko@users.noreply.github.com>
2022-10-29 18:57:38 +0300

    fix: fix CSS rule 3.1

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className="task";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
