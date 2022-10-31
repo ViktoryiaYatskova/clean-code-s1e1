34bcc8da7ecaa349e5391a34cc8fb85113d20e15
Frost <svet.tyshchyk@gmail.com>
2022-10-31 11:21:29 +0200

    init: start clean code task

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Document is the DOM can be accessed in the console with document.window.
 // Tree is from the top, html, body, p etc.
-
+alert("please check task 02.10, thanks")
 //Problem: User interaction does not provide the correct results.
 //Solution: Add interactivity so the user can manage daily tasks.
 //Break things down into smaller steps and take each step at a time.
