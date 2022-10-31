ba6dd39bec19b93fcc99e766835afd2f8d5e1861
sluzerpp <bistboy@list.ru>
2022-10-31 10:16:00 +0300

    refactor: rename css selectors, add margin, add space after property name, add semicolons, add separation

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task__text";
+    editInput.className="task__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="task__edit";
