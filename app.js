c6a10df1d3ccd1c9867138658cf58e663ac8caa3
llallonen <llallonen@gmail.com>
2022-11-03 01:34:09 +0500

    refactor: update script with new selectors in html

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="btn btn--delete";
   deleteButtonImg.src="./remove.svg";
+  deleteButtonImg.className="btn__img";
   deleteButton.appendChild(deleteButtonImg);
 
 
