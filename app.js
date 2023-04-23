33c9306a72b8e8a5d1afd166e2109e458a8130f2
dmitriy3305 <dmitrijkrymskij6@gmail.com>
2023-04-23 23:34:23 +0400

    refactor: changed classes in app.js

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.querySelector('[data-task="new-task"]');//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompletetasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.querySelector(".todo-list");//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector(".completed-list");//completed-tasks
 
 
 //New task list item
