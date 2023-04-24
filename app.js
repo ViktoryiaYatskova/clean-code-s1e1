e4b938e34a4612932d89cb03d574cf735a9da9c4
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2023-04-24 20:27:45 +0300

    refactor:add BEM notation for forming class names

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='to-do_task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="to-do_task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
