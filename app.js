da45742c51b6d96ee9218075e983234faf67aee5
dmitriy3305 <dmitrijkrymskij6@gmail.com>
2023-04-24 20:52:00 +0400

    refactor: rule 3.1 Unified style of naming selectors

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
-    deleteButtonImg.className = 'delete__svg';
+    deleteButtonImg.className = 'delete-svg';
     deleteButton.appendChild(deleteButtonImg);
     
 
